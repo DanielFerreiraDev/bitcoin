080df3d6b49c4d261d7a267db9bafb7b20eca258
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-06-28 17:56:44 -0300

    Adicionando dependências do mysql e hibernate, configurando datasource e criando entidades

---
+++ src/main/java/br/com/alura/model/Ordem.java
@@ -0,0 +1,65 @@
+package br.com.alura.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Ordem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double preco;
+
+    private String tipo;
+
+    private LocalDate data;
+
+    private String status;
+
+    private Long user_id;
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+}
