5000e56e1dca3be9ed1f23bdfab9c65575d5d3ec
DanielFerreiraDev <ferreira-estudante@hotmail.com>
2022-06-30 16:32:22 -0300

    Listando usuários e ordens, corrigindo propriedade do datasource

--- src/main/java/br/com/alura/model/Usuario.java
+++ src/main/java/br/com/alura/model/Usuario.java
@@ -7,6 +7,7 @@ import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
 
+import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -33,6 +34,10 @@ public class Usuario extends PanacheEntityBase {
     @Roles
     private String role;
 
+    public String getNome() {
+        return nome;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -49,6 +54,7 @@ public class Usuario extends PanacheEntityBase {
         this.username = username;
     }
 
+    @JsonbTransient
     public String getPassword() {
         return password;
     }
--- src/main/java/br/com/alura/resource/UsuarioResource.java
+++ src/main/java/br/com/alura/resource/UsuarioResource.java
@@ -3,9 +3,11 @@ package br.com.alura.resource;
 import br.com.alura.model.Usuario;
 
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
 @Path("/usuarios")
 public class UsuarioResource {
@@ -16,5 +18,12 @@ public class UsuarioResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public void inserir(Usuario usuario) {
         Usuario.adicionar(usuario);
+    }
+
+    @GET
+    @RolesAllowed("admin")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Usuario> listar() {
+        return Usuario.listAll();
     }
 }
